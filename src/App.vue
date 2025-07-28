f1073df059763c54d0aae099e76deaa603cafea6
gabriel <gabrielscarpino@hotmail.com>
2025-07-28 17:28:39 -0300

    commit inicial

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { reactive } from 'vue';
+
+const estado = reactive({
+    contador: 0,
+})
+
+function incrementar() {
+    estado.contador++;
+}
+
+function decrementar() {
+    estado.contador--;
+}
+
+const nomes = ['Gabriel', 'Laura', 'Paulo', 'Helena'];
+</script>
+
+<template>
+    <h1>Calculadora VueJS</h1>
+
+    {{ estado.contador }}
+
+    <button @click="incrementar" type="button">+</button>
+    <button @click="decrementar" type="button">-</button>
+    <br>
+    <ul>
+        <li v-for="nome in nomes"> {{nome}} </li>
+    </ul>
+</template>
+
+<style scoped></style>
