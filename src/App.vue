4b034fb99eae26c07559110314c402af6fcf0184
gabriel <gabrielscarpino@hotmail.com>
2025-08-11 18:18:02 -0300

    Deixando a lista de tarefas em branco

--- src/App.vue
+++ src/App.vue
@@ -8,18 +8,7 @@ const estado = reactive({
     filtro: 'todas',
     tarefaTemp: '',
     tarefas: [
-        {
-            titulo: 'Estudar ES6',
-            finalizada: false,
-        },
-        {
-            titulo: 'Estudar SASS',
-            finalizada: false,
-        },
-        {
-            titulo: 'Ir para a academia',
-            finalizada: true,
-        }
+        
     ]
 })
 
