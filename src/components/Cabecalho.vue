0f1cbba6e9ef836aa01d561fd542d7843defc8b4
gabriel <gabrielscarpino@hotmail.com>
2025-08-12 17:09:12 -0300

    Adicionando o v-if ao cabeçalho

--- src/components/Cabecalho.vue
+++ src/components/Cabecalho.vue
@@ -5,8 +5,9 @@
 <template>
     <header class="p-5 mb-4 mt-4 text-center bg-light rounded-3">
         <h1>Lista de tarefas</h1>
-        <p>
-            Você possui {{ props.tarefasPendentes}} tarefas pendentes.
+        <p v-if="props.tarefasPendentes > 0">
+            Você possui {{ props.tarefasPendentes }} tarefas pendentes
         </p>
+        <p v-else>Você não tem tarefas pendentes!</p>
     </header>
 </template>
